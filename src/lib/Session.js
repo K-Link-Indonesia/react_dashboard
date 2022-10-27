952f832ac99e90c0e1b101f728bc673844f5cafe
daintly.klink <daintly.klink@gmail.com>
2022-10-27 09:57:36 +0700

    Clean session

--- src/lib/Session.js
+++ src/lib/Session.js
@@ -1,25 +1,9 @@
 export default function Session() {
-  /*
-  const gset = (field,value) => {
-    sessionStorage.setItem(field,value);
-    // Also set this in cookie/localStorage
-  }
 
-  var get = function(field){
-    return sessionStorage.getItem(field);;    // Or pull this from cookie/localStorage
-  };
-
-  var set = function(field,value){
-    sessionStorage.setItem(field,value);
-    // Also set this in cookie/localStorage
-  };
-  */
 }
-var arrsession=[];
+
 export function SessionSet(field,value){
   sessionStorage.setItem(field,value);
-  arrsession.push(field);
-  // Also set this in cookie/localStorage
 }
 export function SessionGet(field){
   return sessionStorage.getItem(field);
